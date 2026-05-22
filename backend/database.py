5d92d022284520316082f16b1780dc1b391076cf
Ruturaj-Creates <ruturajdesai40@gmail.com>
2026-05-22 21:58:31 +0530

    add environment configuration

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-url="postgresql://postgres:postgres@localhost:54320/TeluskoDB"
+
 engine=create_engine(url)
 session=sessionmaker(bind=engine,autoflush=False)
 
